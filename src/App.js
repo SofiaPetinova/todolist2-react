e12e1e5ea5f86a736c45acd6fdf9b6d690906799
Sofia Petinova <petinova@bk.ru>
2022-09-27 09:08:37 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [list, setList] = useState([
+    {
+      id: uuidv4(),
+      title: 'Learn React',
+      isDone: true
+    },
+    {
+      id: uuidv4(),
+      title: 'Learn Redux',
+      isDone: false
+    },
+    {
+      id: uuidv4(),
+      title: 'Practice',
+      isDone: false
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
